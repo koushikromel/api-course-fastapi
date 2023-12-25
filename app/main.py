10840aed6a21f338c051d6e1aed2ecccaabacb0d
Koushik Romel <koushikromel@gmail.com>
2023-12-25 10:56:58 +0530

    Before encrypting

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, status, Response, HTTPException, Depends
-from pydantic import BaseModel
-from typing import Optional
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from typing import List
 import time
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 
@@ -13,15 +12,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-class UpdatePost(BaseModel):
-    title: str
-    content: str
 
 while True:
     try:
@@ -39,31 +29,29 @@ while True:
 def root():
     return {"message": "Hello, World!"}
 
-@app.get("/sqlalchemy")
-def test_post(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {'data': posts}
-
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
-@app.post("/post/create", status_code=201)
-def create_post(new_post: Post, db: Session = Depends(get_db)):
+@app.post("/post/create", status_code=201, response_model=schemas.Post)
+def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    print('asdf', new_post)
+    print(type(new_post))
+
     # created_post = models.Post(title=new_post.title, content=new_post.content, published=new_post.published)
     created_post = models.Post(**new_post.dict())
     db.add(created_post)
     db.commit()
     db.refresh(created_post)
-    return {"data": created_post}
+    return created_post
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if post is None:
         raise HTTPException(status_code=404, detail=f"Post with id: {id} not found")
-    return {"data": post}
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -74,8 +62,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=204)
 
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     
@@ -83,6 +71,14 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail=f"Post with id: {id} not found")
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post_query.first()}
+    return post_query.first()
 
 # check put vs patch practically
+
+@app.post("/users", status_code=201, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.types import TIMESTAMP
 from sqlalchemy.sql.expression import text
-
 from .database import Base
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -11,5 +11,13 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
 
